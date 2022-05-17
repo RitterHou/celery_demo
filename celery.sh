376277de118d4b7603886119c9a66256e72d74c4
侯锐 <hourui@qianmi.com>
2022-05-17 17:20:26 +0800

    优化celery启动命令

--- celery.sh
+++ celery.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-if [[ "$1" == "-q" ]]; then
-  celery -A search worker -Q sort,celery -l info
-else
-  celery -A search worker -l info
-fi
+operation='worker'
+queue_flag=''
+log_level='info'
+hostname=''
+
+while getopts 'fql:n:' flag; do
+  case "${flag}" in
+  f) operation='flower --port=5555' ;;  # 启动flower，而不是worker
+  q) queue_flag='-Q sort,celery' ;;     # 使用自定义队列
+  l) log_level="${OPTARG}" ;;           # 设置日志级别
+  n) hostname="--hostname ${OPTARG}" ;; # 指定worker的主机名，可选参数：%p，%h，%n，%d
+  *) echo "Unknown flag: ${flag}" ;;
+  esac
+done
+
+celery -A search $operation $queue_flag -l $log_level $hostname
 
 # -A, --app
 # 表示使用的应用实例，设置后会按顺序自动寻找：search.app、search.celery、search.celery.app、search.celery.celery，作为celery的对象
