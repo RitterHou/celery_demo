6d40ac9ca23485941ecda00ecd39d90655e233a9
侯锐 <hourui@qianmi.com>
2022-05-18 16:31:43 +0800

    添加对应用的配置修改

--- start.py
+++ start.py
@@ -2,7 +2,19 @@
 """
 通过Python启动celery的worker
 """
+import logging
+
 from search import app
 
 if __name__ == '__main__':
+    logging.info(app.conf.timezone)
+
+    app.conf.update(timezone='Europe/London')
+    logging.info(app.conf.timezone)
+
+    app.conf.timezone = 'Asia/Shanghai'
+    logging.info(app.conf.timezone)
+
+    logging.info(app.conf.humanize(with_defaults=False, censored=True))
+
     app.worker_main(('worker', '-Q', 'queue_1,celery', '-n', 'python_host', '-l', 'info'))
